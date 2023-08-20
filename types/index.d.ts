2d6a752de85a1e2efef231fc098cb39017cbf6db
Raine Revere <raine@cybersemics.org>
2023-08-20 14:27:11 +0000

    Reorganize folder structure.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 export = spawnPlease;
 /** Spawns a child process, as long as you ask nicely.
+ *
  * @param {string} command - The shell command to execute.
  * @param {string[]} args - An array of arguments that are given after the command.
  * @param {string | any} [stdin] - A string that is passed to stdin.
